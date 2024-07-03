957ded55f67f9c57bb7f9b55ca1be2c52991c89c
Yasser Gawish <yasser.gawish@s44.team>
2024-07-03 23:54:52 +0300

    First Assignment: Add customer controller

---
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbrewery.web.model;
+
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CustomerDto {
+
+    private UUID id;
+
+    private String name;
+
+}
